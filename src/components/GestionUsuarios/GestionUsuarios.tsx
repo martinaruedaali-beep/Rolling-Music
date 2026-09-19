75369da5e9b9da54b5c155198518663c4956f107
zabdielpuma <ibarpuma@gmail.com>
2026-09-19 00:49:06 -0300

    agrega la funcion de editar los usuarios registrados

--- src/components/GestionUsuarios/GestionUsuarios.tsx
+++ src/components/GestionUsuarios/GestionUsuarios.tsx
@@ -1,53 +1,68 @@
 import React from "react";
 import { type Usuario } from "../../types/usuario";
 import { useState } from "react";
-import { DeleteOutlined} from "@ant-design/icons";
-import{ Modal } from "antd";
-import "../GestionUsuarios/GestionUsuarios.css"
-
-const datosIniciales: Usuario[] = [
-  {
-    id: 1,
-    nombre: "Juan",
-    apellido: "Pérez",
-    email: "juan@mail.com",
-    password: "contraseña123",
-    rol: "Usuario",
-  },
-  {
-    id: 2,
-    nombre: "María",
-    apellido: "Gómez",
-    email: "maria@mail.com",
-    password: "contraseña456",
-    rol: "Usuario",
-  },
-  {
-    id: 3,
-    nombre: "Admin",
-    apellido: "Rock",
-    email: "admin@rock.com",
-    password: "admincontraseña",
-    rol: "Admin",
-  },
-];
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Modal } from "antd";
+import {
+  obtenerUsuarios,
+  guardarUsuarios,
+  actualizarUsuarios,
+} from "../../services/usuarioservice";
+import "../GestionUsuarios/GestionUsuarios.css";
 
 const GestionUsuarios: React.FC = () => {
+  const [usuarios, setUsuarios] = useState<Usuario[]>(obtenerUsuarios());
 
-  const [usuario, setUsuario] = useState<Usuario[]>(datosIniciales);
-  const eliminarUsuario = (id:number): void => {
+  const [usuarioEditando, setUsuarioEditando] = useState<Usuario | null>(null);
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [email, setEmail] = useState("");
+  const [rol, setRol] = useState("");
+
+  const editarUsuario = (usuario: Usuario): void => {
+    setUsuarioEditando(usuario);
+    setNombre(usuario.nombre);
+    setApellido(usuario.apellido);
+    setEmail(usuario.email);
+    setRol(usuario.rol ?? "");
+  };
+
+  const guardarCambios = (): void => {
+    if (!usuarioEditando) return;
+
+    const usuarioActualizado: Usuario = {
+      ...usuarioEditando,
+      nombre,
+      apellido,
+      email,
+      rol: rol as "Admin" | "Usuario" ,
+    };
+
+    actualizarUsuarios(usuarioActualizado);
+    const listaActualizada = usuarios.map((u) =>
+      u.id === usuarioEditando.id ? usuarioActualizado : u
+    );
+
+    setUsuarios(listaActualizada);
+    guardarUsuarios(listaActualizada);
+    setUsuarioEditando(null);
+  };
+
+  const eliminarUsuario = (id: number): void => {
     Modal.confirm({
-    title: '¿Seguro que deseas eliminar este usuario?',
-    content: 'Esta acción no se podrá deshacer.',
-    okText: 'Sí, eliminar',
-    okType: 'danger',
-    cancelText: 'Cancelar',
-    onOk() {
-      setUsuario((prev) => prev.filter((user) => user.id !== id));
-    },
-  });
+      title: "¿Seguro que deseas eliminar este usuario?",
+      content: "Esta acción no se podrá deshacer.",
+      okText: "Sí, eliminar",
+      okType: "danger",
+      cancelText: "Cancelar",
+      onOk() {
+        const usuariosActualizados = usuarios.filter((user) => user.id !== id);
 
-  }
+        setUsuarios(usuariosActualizados);
+        guardarUsuarios(usuariosActualizados);
+      },
+    });
+  };
 
   return (
     <>
@@ -64,26 +79,65 @@ const GestionUsuarios: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-            {usuario.map((usuario)=> (
-                <tr key={usuario.id}>
-                    <td>{usuario.id}</td>
-                    <td>{`${usuario.nombre} ${usuario.apellido}`}</td>
-                    <td>{usuario.email}</td>
-                    <td>{usuario.rol ?? 'Usuario'}</td>
-                    <td>
-                        <button 
-                        type="button"
-                        className="eliminarUsuario"
-                        onClick={() => eliminarUsuario(usuario.id)}>
-                        <DeleteOutlined />
-                        </button>
-                    </td>
-                </tr>
+            {usuarios.map((usuarios) => (
+              <tr key={usuarios.id}>
+                <td>{usuarios.id}</td>
+                <td>{`${usuarios.nombre} ${usuarios.apellido}`}</td>
+                <td>{usuarios.email}</td>
+                <td>{usuarios.rol ?? "Usuario"}</td>
+                <td>
+                  <button
+                    type="button"
+                    className="editarUsuario"
+                    onClick={() => editarUsuario(usuarios)}
+                  >
+                    <EditOutlined />
+                  </button>
+
+                  <button
+                    type="button"
+                    className="eliminarUsuario"
+                    onClick={() => eliminarUsuario(usuarios.id)}
+                  >
+                    <DeleteOutlined />
+                  </button>
+                </td>
+              </tr>
             ))}
-
-
           </tbody>
         </table>
+        <Modal
+          title="editar usuario"
+          open={usuarioEditando !== null}
+          onOk={guardarCambios}
+          onCancel={() => setUsuarioEditando(null)}
+          okText="Guardar cambios"
+          cancelText="Cancelar"
+        >
+          <input
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            placeholder="Nombre"
+          />
+
+          <input
+            value={apellido}
+            onChange={(e) => setApellido(e.target.value)}
+            placeholder="Apellido"
+          />
+
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+
+          <input
+            value={rol}
+            onChange={(e) => setRol(e.target.value)}
+            placeholder="Rol"
+          />
+        </Modal>
       </section>
     </>
   );
--- src/services/usuarioservice.ts
+++ src/services/usuarioservice.ts
@@ -7,7 +7,32 @@ export function obtenerUsuarios(): Usuario[] {
   const usuariosGuardados = localStorage.getItem(CLAVE_USUARIOS)
 
   if (!usuariosGuardados) {
-    return []
+    return [
+      {
+        id: 1,
+        nombre: "Juan",
+        apellido: "Pérez",
+        email: "juan@gmail.com",
+        password: "1234",
+        rol: "Usuario",
+      },
+      {
+        id: 2,
+        nombre: "María",
+        apellido: "Gómez",
+        email: "maria@gmail.com",
+        password: "1234",
+        rol: "Admin",
+      },
+      {
+        id: 3,
+        nombre: "Carlos",
+        apellido: "Rodríguez",
+        email: "carlos@gmail.com",
+        password: "1234",
+        rol: "Usuario",
+      },
+    ];
   }
 
   return JSON.parse(usuariosGuardados) as Usuario[]
@@ -43,4 +68,16 @@ export function crearUsuario(
   guardarUsuarios(usuarios)
 
   return nuevoUsuario
+}
+
+export function actualizarUsuarios(usuarioActualizado: Usuario): Usuario {
+  const usuarios = obtenerUsuarios()
+  const usuarioActualizados = usuarios.map((usuario) => {
+    if (usuario.id === usuarioActualizado.id) {
+      return usuarioActualizado
+    }
+    return usuario
+  })
+  guardarUsuarios(usuarioActualizados)
+  return usuarioActualizado
 }
