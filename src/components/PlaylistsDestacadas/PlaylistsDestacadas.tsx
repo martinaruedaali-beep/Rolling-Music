81feaf05d0e91a464cd1c79b608ab16cf9cca92a
Clarisa <clarisa.catalinas.ledesma@gmail.com>
2026-09-20 21:23:09 -0300

    tsx playlist

---
+++ src/components/PlaylistsDestacadas/PlaylistsDestacadas.tsx
@@ -0,0 +1,23 @@
+import { catalogoAlbums } from '../../data/mockData';
+import './PlaylistsDestacadas.css';
+
+function PlaylistsDestacadas() {
+  return (
+    <section className="destacadas">
+      <h2>Playlists destacadas</h2>
+
+      <div className="destacadas-grilla">
+        {catalogoAlbums.map((album) => (
+          <div className="tarjeta" key={album.id}>
+            <img src={album.coverImage} alt={album.title} />
+            <h3>{album.title}</h3>
+            <p>{album.genre}</p>
+            <p>{album.songs.length} canciones</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default PlaylistsDestacadas;
