92e225c48776b21fd01bcddba77b47cc916e7707
Clarisa <clarisa.catalinas.ledesma@gmail.com>
2026-09-21 00:44:13 -0300

    ruteo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import Inicio from './pages/inicio/inicio'
-import Error404 from './pages/error404/error404'
-import './App.css'
-import Admin from './pages/admin/admin'
-import Registro from './components/Registro/Registro'
-import Login from './components/login/login'
-import Header from './components/Header/Header'
-import PlaylistsDestacadas from './components/PlaylistsDestacadas/PlaylistsDestacadas';
-import Sidebar from './components/Sidebar/Sidebar'
-import Playlist from './components/Playlist'
-import Reproductor from './components/Reproductor/Reproductor'
+import { Routes, Route } from "react-router-dom";
+import Inicio from "./pages/inicio/inicio";
+import Error404 from "./pages/error404/error404";
+import "./App.css";
+import Admin from "./pages/admin/admin";
+import Registro from "./components/Registro/Registro";
+import Login from "./components/login/login";
+import Header from "./components/Header/Header";
+import PlaylistsDestacadas from "./components/PlaylistsDestacadas/PlaylistsDestacadas";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Playlist from "./components/Playlist";
+import Reproductor from "./components/Reproductor/Reproductor";
 
 function App() {
   return (
@@ -30,7 +30,17 @@ function App() {
       <Route path="/admin" element={<Admin />} />
       <Route path="/registro" element={<Registro />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/playlists" element={<PlaylistsDestacadas />} />
+      <Route
+        path="/favoritos"
+        element={
+          <>
+            <Header />
+            <Sidebar />
+            <PlaylistsDestacadas />
+            <Reproductor />
+          </>
+        }
+      />
     </Routes>
   );
 }
