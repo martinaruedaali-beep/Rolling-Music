d2c919a30272daac2a5b866b1e6ab26009857223
callejeros2678 <callejeros2678@gmail.com>
2026-09-21 12:40:19 -0300

    feat: actualizacion,

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { AlbunDetalle as DetalleAlbum } from './components/AlbunDetalle';
-import Admin from './pages/admin/admin'; 
+import Admin from './pages/admin/admin';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        
-        <Route path="/" element={<DetalleAlbum />} />
-        
-        
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<DetalleAlbum />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
   );
 }
 
