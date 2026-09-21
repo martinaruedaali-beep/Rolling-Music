54d314eddf90ae50711f1a004ff8809bdbb8dd9b
Jeronimo Geria <jgeria21@gmail.com>
2026-09-21 12:54:24 -0300

    feat: integra hero con sesion de usuario

--- src/pages/inicio/inicio.tsx
+++ src/pages/inicio/inicio.tsx
@@ -4,24 +4,25 @@ import Sidebar from '../../components/Sidebar/Sidebar'
 import Playlist from '../../components/Playlist'
 import Reproductor from '../../components/Reproductor/Reproductor'
 import Hero from '../../components/hero/hero'
+import { obtenerSesion } from '../../services/sesionservice'
 
 function Inicio() {
   const [mostrarIntro, setMostrarIntro] = useState(true)
+  const sesion = obtenerSesion()  
 
   if (mostrarIntro) {
     return <Intro onFinish={() => setMostrarIntro(false)} />
   }
 
-return (
-  <div className="inicio-layout">
-    <Sidebar />
-    <main className="inicio-content">
-      <Hero />
-      <Playlist />
-    </main>
-    <Reproductor />
-  </div>
-)
-}
-
+  return (
+    <div className="inicio-layout">
+      <Sidebar />
+      <main className="inicio-content">
+        {sesion && <Hero />}
+        <Playlist />
+      </main>
+      <Reproductor />
+    </div>
+  )
+} 
 export default Inicio
