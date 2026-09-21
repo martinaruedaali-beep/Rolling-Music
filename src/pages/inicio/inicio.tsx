79aa170790fcebbe5d35414c8304c9a8347dab37
Jeronimo Geria <jgeria21@gmail.com>
2026-09-21 01:51:28 -0300

    feat: implementa hero principal

--- src/pages/inicio/inicio.tsx
+++ src/pages/inicio/inicio.tsx
@@ -3,6 +3,7 @@ import Intro from '../../components/animacion-intro/intro'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Playlist from '../../components/Playlist'
 import Reproductor from '../../components/Reproductor/Reproductor'
+import Hero from '../../components/hero/hero'
 
 function Inicio() {
   const [mostrarIntro, setMostrarIntro] = useState(true)
@@ -11,13 +12,16 @@ function Inicio() {
     return <Intro onFinish={() => setMostrarIntro(false)} />
   }
 
-  return (
-    <>
-      <Sidebar />
+return (
+  <div className="inicio-layout">
+    <Sidebar />
+    <main className="inicio-content">
+      <Hero />
       <Playlist />
-      <Reproductor />
-    </>
-  )
+    </main>
+    <Reproductor />
+  </div>
+)
 }
 
 export default Inicio
