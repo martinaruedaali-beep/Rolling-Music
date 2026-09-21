f0f33dea4be38448b6da0f368f3fbab7ece64918
callejeros2678 <callejeros2678@gmail.com>
2026-09-21 12:19:50 -0300

    feat: actualizacion de rutas principales en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AlbunDetalle as DetalleAlbum } from './components/AlbunDetalle';
+import Admin from './pages/admin/admin'; 
 import './App.css';
 
 function App() {
   return (
-    <>
-      <DetalleAlbum />
-    </>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<DetalleAlbum />} />
+        
+        
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
